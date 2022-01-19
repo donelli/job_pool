1062aaae40fb2e9770009474d43a8b8586ffd51a
donelli <49295044+donelli@users.noreply.github.com>
2022-01-19 16:40:25 -0300

    update tag names to english

--- src/tagger.py
+++ src/tagger.py
@@ -29,7 +29,7 @@ class Tagger:
       Tag('NodeJs', [ 'NODE.JS', 'NODEJS' ]),
       
       # Apis
-      Tag('Api', [ 'APIS', 'REST', 'WEB SERVICE', 'WEB SERVICES', 'REST APIs' ]),
+      Tag('API', [ 'APIS', 'REST', 'WEB SERVICE', 'WEB SERVICES', 'REST APIs' ]),
       Tag('Webservice', [ 'WEB SERVICE', 'WEB SERVICES', 'WEBSERVICE', 'WEBSERVICES' ]),
       Tag('Postman', [ 'POSTMAN' ]),
       
@@ -48,10 +48,13 @@ class Tagger:
       Tag('Python', [ 'PYTHON' ]),
       Tag('C# / .NET', [ 'C#', '.NET' ]),
       Tag('Golang', [ 'GO', 'GOLANG' ]),
+      
+      # Operating Systems
+      Tag('Linux', [ 'LINUX' ]),
 
-      # Banco de dados
-      Tag('DBs Relacionais', [ 'BANCO DE DADOS RELACIONAL', 'SQL' ]),
-      Tag('DBs não relacionais', [ 'NAO RELACIONAL' ]),
+      # Databases
+      Tag('SQL', [ 'BANCO DE DADOS RELACIONAL', 'SQL' ]),
+      Tag('NoSQL', [ 'NAO RELACIONAL', 'NOSQL' ]),
       Tag('SqlServer', [ 'SQLSERVER' ]),
       Tag('Postgres', [ 'POSTGRES', 'POSTGRESQL' ]),
       Tag('Mongo', [ 'MONGODB', 'MONGO' ]),
@@ -61,41 +64,38 @@ class Tagger:
       Tag('Docker', [ 'DOCKER' ]),
       Tag('Kubernetes', [ 'KUBERNETES' ]),
       
-      # Teoria
-      Tag('Metod. Ágeis', [ 'AGEIS', 'SCRUM', 'KANBAN' ]),
+      # Teory
+      Tag('Agile Develop.', [ 'AGEIS', 'SCRUM', 'KANBAN' ]),
       Tag('Design Patterns', [ 'DESIGN PATTERNS' ]),
       Tag('Clean Code', [ 'CLEAN CODE' ]),
-      Tag('Estruturas de dados', [ 'ESTRUTURAS DE DADOS' ]),
+      Tag('Data Structure', [ 'ESTRUTURAS DE DADOS' ]),
       Tag('CI/CD', [ 'CI / CD', 'CI/CD' ]),
-      Tag('Gestão de Projetos', [ 'GESTAO DE PROJETOS' ]),
-      Tag('Gestão de Produtos', [ 'GESTAO DE PRODUTOS' ]),
-      Tag('Segurança', [ 'SEGURANCA' ]),
-      Tag('Regras de negócio', [ 'REGRAS DE NEGOCIO' ]),
-      Tag('Análise de requisitos', [ 'ANALISE DE REQUISITOS' ]),
-      Tag('Analista de Qualidade', [ 'ANALISTA DE QUALIDADE' ]),
-      Tag('Gerenc. dependências', [ 'GERENCIAMENTO DE DEPENDÊNCIAS' ]),
-      Tag('Sistemas Distribuídos', [ 'SISTEMAS DISTRIBUÍDOS' ]),
-      Tag('Microsserviços', [ 'MICROSSERVIÇOS', 'MICRO SERVIÇOS' ]),
+      Tag('Project management', [ 'GESTAO DE PROJETOS' ]),
+      Tag('Product management', [ 'GESTAO DE PRODUTOS' ]),
+      Tag('Securiry', [ 'SEGURANCA' ]),
+      Tag('Requirements analysis', [ 'ANALISE DE REQUISITOS' ]),
+      Tag('Quality analysis', [ 'ANALISTA DE QUALIDADE' ]),
+      Tag('Dependence analysis', [ 'GERENCIAMENTO DE DEPENDÊNCIAS' ]),
+      Tag('Distributed systems', [ 'SISTEMAS DISTRIBUÍDOS' ]),
+      Tag('Microservices', [ 'MICROSSERVIÇOS', 'MICRO SERVIÇOS' ]),
       
-      # Testes
-      Tag('Testes Unitários', [ 'TESTES UNITARIOS' ]),
-      Tag('Testes Automatizados', [ 'TESTES AUTOMATIZADOS', 'AUTOMACAO DE TESTES' ]),
-      Tag('Testes', [ 'PLANO DE TESTES', 'CENARIOS DE TESTE' ]),
+      # Tests
+      Tag('Unit testing', [ 'TESTES UNITARIOS' ]),
+      Tag('Automated testing', [ 'TESTES AUTOMATIZADOS', 'AUTOMACAO DE TESTES' ]),
+      Tag('Tests', [ 'PLANO DE TESTES', 'CENARIOS DE TESTE' ]),
       Tag('Cypress', [ 'CYPRESS' ]),
-      Tag('Testes Web', [ 'TESTES WEB' ]),
-      Tag('Testes API', [ 'TESTES DE API' ]),
+      Tag('Web tests', [ 'TESTES WEB' ]),
+      Tag('API tests', [ 'TESTES DE API' ]),
       
-      # Data science
-      Tag('Transformação de dados', [ 'TRANSFORMACAO DE DADOS', 'MANIPULACAO DE DADOS' ]),
-      
-      # Outros
+      # Others
+      Tag('ETL', [ 'TRANSFORMACAO DE DADOS', 'MANIPULACAO DE DADOS' ]),
       Tag('Git', [ 'GIT', 'GITHUB' ]),
       Tag('Bundlers', [ 'BUNDLERS', 'WEBPACK', 'BABEL' ]),
       Tag('Front End', [ 'FRONT-END', 'FRONT END', 'FRONTEND' ]),
-      Tag('Mensagerias', [ 'MENSAGERIAS', 'RABBITMQ', 'AZURESERVICEBUS' ]),
+      Tag('Message broker', [ 'MENSAGERIAS', 'RABBITMQ', 'AZURESERVICEBUS' ]),
       Tag('Azure', [ 'AZURE' ]),
       Tag('SAP', [ 'SAP' ]),
-      Tag('Machine Learning', [ 'MACHINE LEARNING' ]),
+      Tag('Machine learning', [ 'MACHINE LEARNING' ]),
       Tag('Pipelines', [ 'PIPELINES' ]),
       Tag('Azure Datafactory', [ 'AZURE DATAFACTORY' ]),
       Tag('AWS', [ 'AWS' ]),
