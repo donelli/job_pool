6c4cce93ef0f8231edacd37bc0954bde9fde77e7
donelli <49295044+donelli@users.noreply.github.com>
2022-02-09 08:09:15 -0300

    transform Tagger into a singleton

--- src/tagger.py
+++ src/tagger.py
@@ -3,13 +3,20 @@ import re
 from typing import List
 from unidecode import unidecode
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 class Tag:
    
    def __init__(self, name: str, words: List[str]):
       self.name = name
       self.words = words
 
-class Tagger:
+class Tagger(metaclass=Singleton):
    
    tags = [
       
