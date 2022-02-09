7ad04ef08ed7acf393ac611d774110b710cf007a
donelli <49295044+donelli@users.noreply.github.com>
2022-02-09 13:14:26 -0300

    added list of valid user agents

--- src/helpers.py
+++ src/helpers.py
@@ -34,13 +34,17 @@ def removeParamsFromLink(link: str):
 
 def waitRandom():
   time.sleep(random.randint(10, 25))
+userAgents = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.81 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.81 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.81 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'
+]
 
 def getRandomRequestHeaders():
   
-  ua = pyuser_agent.UA()
-
   headers = {
-    "User-Agent" : ua.random
+    "User-Agent" : userAgents[random.randint(0, len(userAgents) - 1)],
   }
 
   return headers
