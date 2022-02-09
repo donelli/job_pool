932054ec91308b3d2786d1770af7e07fe82e6f8b
donelli <49295044+donelli@users.noreply.github.com>
2022-02-09 12:37:19 -0300

    add default values for ElementNotFound Exception

--- src/exceptions/element_not_found.py
+++ src/exceptions/element_not_found.py
@@ -5,7 +5,7 @@ class ElementNotFoundException(Exception):
    Element not found exception
    """
    
-   def __init__(self, element_name, class_, id):
+   def __init__(self, element_name, class_ = "", id = ""):
       self.element_name = element_name
       self.class_ = class_
       self.id = id
